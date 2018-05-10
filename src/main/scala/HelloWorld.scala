49c2d6f858e8692c5119ea0814c3a11cb424ffda
Mark Vallis <markvallis1987@Gmail.com>
2018-05-11 00:31:10 +0100

    added method call example

--- src/main/scala/HelloWorld.scala
+++ src/main/scala/HelloWorld.scala
@@ -1,6 +1,12 @@
 object HelloWorld {
 
-  def main(args: Array[String]): Unit ={
-    println("Hello world")
+  def main(args: Array[String]): Unit = {
+    println("Hello world " + sum(4,3))
+  }
+
+  def sum(a: Int, b: Int) : Int = {
+    var sum:Int = 0
+    sum = a + b
+    sum
   }
 }
