2c50dfb8d05a875348a292ddc2d75f70be7bc8fb
mark <markvallis1987@Gmail.com>
2018-06-23 19:25:47 +0100

    Tidied up console output

--- src/main/scala/Basics/Loops_and_Recursion.scala
+++ src/main/scala/Basics/Loops_and_Recursion.scala
@@ -7,6 +7,7 @@ object Loops_and_Recursion {
     whileLoop(i)
     doWhileLoop(i)
     forLoop()
+    iterateString()
     iterateList(List(1, 7, 4, 2, 12))
     forLoopWithConditional()
     nestedForLoop()
@@ -16,51 +17,60 @@ object Loops_and_Recursion {
     println("\nWhile loop")
     var i = n
     while (i < 10) {
-      println(i)
+      print(i + "\t")
       i += 1
     }
+    print("\n")
   }
 
   def doWhileLoop(n: Int): Unit = {
     println("\nDo-While loop")
     var i = n
     do {
-      println(i)
+      print(i + "\t")
       i -= 1
     } while (i > 0)
+    print("\n")
   }
 
   def forLoop(): Unit = {
     println("\nFor loop")
     for (i <- 1 to 10) {
-      println(i)
+      print(i + "\t")
     }
+    print("\n")
+  }
 
-    println("\nFor loop - over a string")
+  def iterateString(): Unit = {
+    println("\nIterating over a String")
     val randLetters = "ABCDEFGHIJKLM"
     for (i <- 0 until randLetters.indexOf("J"))
-      println(randLetters(i))
+      print(randLetters(i) + "\t")
+    print("\n")
   }
 
   def iterateList(aList: List[Int]): Unit = {
-    println("\nIterating over a list")
+    println("\nIterating over a List")
     for (i <- aList)
-      println(i)
+      print(i + "\t")
+    print("\n")
   }
 
   def forLoopWithConditional(): Unit = {
-    println("\nLoops with conditionals")
+    println("\nLoop with conditionals")
     var evenList = for {i <- 1 to 20
                         if (i % 2) == 0
     } yield i
 
-    iterateList(evenList.toList)
+    for (i <- evenList)
+      print(i + "\t")
+    print("\n")
   }
 
   def nestedForLoop(): Unit = {
     println("\nNested for loops")
     for (i <- 1 to 6; j <- 2 to 7)
-      println("i: " + i + " j: " + j)
-
+      println("i = " + i + " j = " + j)
+    print("\n")
   }
 }
