6b7cea786f6f36260214ac5d42029542d7d8674e
mark <markvallis1987@Gmail.com>
2018-06-23 19:11:21 +0100

    Added list iteration

--- src/main/scala/Basics/Loops_and_Recursion.scala
+++ src/main/scala/Basics/Loops_and_Recursion.scala
@@ -3,9 +3,12 @@ package Basics
 object Loops_and_Recursion {
 
   def main(args: Array[String]): Unit = {
-    var i = 0
+    val i = 0
     whileLoop(i)
     doWhileLoop(i)
+    forLoop()
+    iterateList(List(1, 7, 4, 2, 12))
+    forLoopWithConditional()
   }
 
   def whileLoop(n: Int): Unit = {
@@ -24,5 +27,32 @@ object Loops_and_Recursion {
       println(i)
       i -= 1
     } while (i > 0)
+  }
+
+  def forLoop(): Unit = {
+    println("\nfor loops")
+    for (i <- 1 to 10) {
+      println(i)
+    }
+
+    println("\nfor loop - over string chars")
+    val randLetters = "ABCDEFGHIJKLM"
+    for (i <- 0 until randLetters.indexOf("J"))
+      println(randLetters(i))
+  }
+
+  def iterateList(aList: List[Int]): Unit = {
+    println("\niterating over a list")
+    for (i <- aList)
+      println(i)
+  }
+
+  def forLoopWithConditional(): Unit = {
+    println("\nloops with conditionals")
+    var evenList = for {i <- 1 to 20
+                        if (i % 2) == 0
+    } yield i
+
+    iterateList(evenList.toList)
   }
 }
