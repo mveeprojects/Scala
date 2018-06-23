cde214171b2674c948ffa06616f5d87541798ee4
mark <markvallis1987@Gmail.com>
2018-06-23 18:07:46 +0100

    Renamed method for easier reading

--- src/main/scala/Basics/Conditional_and_Logical_Operators.scala
+++ src/main/scala/Basics/Conditional_and_Logical_Operators.scala
@@ -6,10 +6,10 @@ object Conditional_and_Logical_Operators {
 
   def main(args: Array[String]): Unit = {
     age = 17
-    println("you " + (if (ifElseExample()) "can" else "cannot") + " vote")
+    println("you " + (if (isOldEnough()) "can" else "cannot") + " vote")
   }
 
-  def ifElseExample(): Boolean = {
+  def isOldEnough(): Boolean = {
     var result = false
     if (age >= 18) {
       result = true
