b17ccef6dd2719388ab32539149e2b547940affc
mark <markvallis1987@Gmail.com>
2018-06-23 18:03:43 +0100

    Added example of ternary operator

--- src/main/scala/Basics/Conditional_and_Logical_Operators.scala
+++ src/main/scala/Basics/Conditional_and_Logical_Operators.scala
@@ -5,19 +5,16 @@ object Conditional_and_Logical_Operators {
   var age = 0
 
   def main(args: Array[String]): Unit = {
-    age = 21
-    val canVote = ifElseExample()
-    println("you " + canVote + " vote")
+    age = 17
+    println("you " + (if (ifElseExample()) "can" else "cannot") + " vote")
   }
 
-  def ifElseExample(): String = {
-    var result = ""
+  def ifElseExample(): Boolean = {
+    var result = false
     if (age >= 18) {
-      result = "can"
-    } else if (age < 18 && age > 0) {
-      result = "cannot"
+      result = true
     } else {
-      result = "shouldn't"
+      result = false
     }
     result
   }
