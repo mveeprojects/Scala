cfabfa2bdd88c7d2a4003f10a842d96851635d45
mark <markvallis1987@Gmail.com>
2018-06-24 19:47:11 +0100

    Abstraction example updated

--- src/main/scala/MainConcepts/Abstraction/Abstraction.scala
+++ src/main/scala/MainConcepts/Abstraction/Abstraction.scala
@@ -2,4 +2,23 @@ package MainConcepts.Abstraction
 
 object Abstraction {
 
+  def main(args: Array[String]): Unit = {
+    val fang = new Wolf("Fang")
+    fang.moveSpeed = 36.0
+    println(fang.move)
+  }
+
+  abstract class Mammal(val name: String) {
+    var moveSpeed: Double
+
+    def move: String
+  }
+
+  class Wolf(name: String) extends Mammal(name){
+    override var moveSpeed: Double = 35.0
+
+    override def move: String = {
+      "The wolf %s runs %.2f mph".format(this.name, this.moveSpeed)
+    }
+  }
 }
