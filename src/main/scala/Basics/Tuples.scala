1b812d69204ea97583cd086fe21e12195e556f77
mark <markvallis1987@Gmail.com>
2018-06-24 17:00:21 +0100

    added tuple example

---
+++ src/main/scala/Basics/Tuples.scala
@@ -0,0 +1,19 @@
+package Basics
+
+object Tuples {
+
+  def main(args: Array[String]): Unit = {
+    tupleExample()
+  }
+
+  def tupleExample(): Unit = {
+    val tupleMarge = (103, "Marge Simpson", 10.50000)
+    printf("%s owes us £%.2f\n\n", tupleMarge._2, tupleMarge._3)
+
+    printTuple(tupleMarge)
+  }
+
+  def printTuple(tuple: (Int, String, Double)): Unit = {
+    tuple.productIterator.foreach{i => println(i)}
+  }
+}
