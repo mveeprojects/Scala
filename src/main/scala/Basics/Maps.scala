ddaa6bf970f8543b2fb32973feb1f4b602b8fb3f
mark <markvallis1987@Gmail.com>
2018-06-24 16:35:22 +0100

    added maps examples

---
+++ src/main/scala/Basics/Maps.scala
@@ -0,0 +1,44 @@
+package Basics
+
+import scala.collection.mutable
+
+object Maps {
+
+  val employees = Map(
+    "Manager" -> "Bob",
+    "Secretary" -> "Linda"
+  )
+
+  val customers = collection.mutable.Map(
+    1 -> "Paul",
+    2 -> "Deborah"
+  )
+
+  def main(args: Array[String]): Unit = {
+    immutableMaps()
+    mutableMaps()
+  }
+
+  def immutableMaps(): Unit = {
+    if (employees.contains("Manager")) {
+      printf("manager: %s\n", employees("Manager"))
+    }
+    print("\n")
+  }
+
+  private def mutableMaps(): Unit = {
+    printf("Customer 1: %s\n", customers(1))
+    printf("Customer 2: %s\n", customers(2))
+
+    customers(1) = "Tom"
+    customers(3) = "Sally"
+
+    printMap(customers)
+  }
+
+  private def printMap(map: mutable.Map[Int, String]): Unit = {
+    for ((k, v) <- map) {
+      printf("%d: %s\n", k, v)
+    }
+  }
+}
