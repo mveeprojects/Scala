e3840bfd09b45ed24b826b5b6d488fb43ae4e57c
mark <markvallis1987@Gmail.com>
2018-06-24 17:28:55 +0100

    encapsulation example working

--- src/main/scala/MainConcepts/Encapsulation/Encapsulation.scala
+++ src/main/scala/MainConcepts/Encapsulation/Encapsulation.scala
@@ -1,5 +1,64 @@
 package MainConcepts.Encapsulation
 
 object Encapsulation {
+  def main(args: Array[String]): Unit = {
+
+    var animal = new Animal()
+    println(animal.getSound())
+    println(animal.toString)
+
+    var anotherAnimal = new Animal("Bob", "boo")
+    println(anotherAnimal.getSound())
+    println(anotherAnimal.toString)
+  }
+
+  class Animal(var name: String, var sound: String) {
+
+    this.setName(name)
+
+    val id: Int = Animal.newIdNumber
+
+    def this(name: String){
+      this("No name set", "No sound set")
+      this.setName(name)
+    }
+
+    def this(){
+      this("No name set", "No sound set")
+    }
+
+    def getName: String = {
+      name
+    }
+
+    def getSound: String = {
+      sound
+    }
+
+    def setName(name: String): Unit = {
+      if (!name.matches(".*\\d+.*")) {
+        this.name = name
+      } else {
+        this.name = "No name set"
+      }
+    }
+
+    def setSound(sound: String): Unit = {
+      this.sound = sound
+    }
+
+    override def toString: String = {
+      "%s with id %d says %s".format(this.name, this.id, this.sound)
+    }
+  }
+
+  // static companion object
+  object Animal {
+    private var idNumber = 0
+    private def newIdNumber = {
+      idNumber += 1
+      idNumber
+    }
+  }
 
 }
--- src/main/scala/build.sbt
+++ src/main/scala/build.sbt
@@ -6,4 +6,5 @@ scalaVersion := "2.12.6"
 
 lazy val Basics = project
 lazy val Maths = project
-lazy val MainConcepts = project
+lazy val Encapsulation = project in file("MainConcepts")
+lazy val Classes = project
