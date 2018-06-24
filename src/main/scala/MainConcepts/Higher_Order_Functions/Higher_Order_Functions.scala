ea526e21d1f394c910590422efe813843e70dbea
mark <markvallis1987@Gmail.com>
2018-06-24 20:31:41 +0100

    higher order functions (mapping, filtering and passing functions into other functions) examples added

---
+++ src/main/scala/MainConcepts/Higher_Order_Functions/Higher_Order_Functions.scala
@@ -0,0 +1,31 @@
+package MainConcepts.Higher_Order_Functions
+
+object Higher_Order_Functions {
+
+  val testList = List(1, 3, 5, 7, 11)
+
+  def main(args: Array[String]): Unit = {
+    mapping()
+    filtering()
+    printf("3 * 100 = %.1f\n\n", passingFunctionIntoFunction(times3, 100))
+  }
+
+  private def mapping(): Unit = {
+    println("Mapping example")
+    testList.map((i: Int) => i * 10).foreach(println)
+    print("\n")
+  }
+
+  private def filtering(): Unit = {
+    println("Filtering example")
+    testList.filter(_ % 3 == 0).foreach(println)
+    print("\n")
+  }
+
+  private def times3(num: Int) = num * 3
+
+  private def passingFunctionIntoFunction(function: Int => Double, num: Int) = {
+    println("Passing functions into functions")
+    function(num)
+  }
+}
