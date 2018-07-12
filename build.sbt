3728693b615d67bf0cc4cf5a89dca2155b093721
Mark Vallis <markvallis1987@Gmail.com>
2018-07-12 19:41:34 +0100

    Updated build.sbt and added a more detailed example of Either

--- build.sbt
+++ build.sbt
@@ -2,4 +2,25 @@ name := "Scala"
 
 version := "0.1"
 
-scalaVersion := "2.12.6"
+lazy val akkaHttpVersion = "10.0.10"
+lazy val akkaVersion    = "2.5.4"
+
+lazy val root = (project in file(".")).
+  settings(
+    inThisBuild(List(
+      organization    := "com.mark.stuff",
+      scalaVersion    := "2.12.6"
+    )),
+    name := "aiengine",
+    libraryDependencies ++= Seq(
+      "com.typesafe.akka" %% "akka-http"            % akkaHttpVersion,
+      "com.typesafe.akka" %% "akka-http-spray-json" % akkaHttpVersion,
+      "com.typesafe.akka" %% "akka-stream"          % akkaVersion,
+      "com.typesafe.akka" %% "akka-slf4j" % akkaVersion,
+      "com.typesafe.akka" %% "akka-http-testkit"    % akkaHttpVersion % Test,
+      "com.typesafe.akka" %% "akka-testkit"         % akkaVersion     % Test,
+      "com.typesafe.akka" %% "akka-stream-testkit"  % akkaVersion     % Test,
+      "org.scalatest"     %% "scalatest"            % "3.0.1"         % Test,
+      "com.typesafe.akka" %% "akka-http-core" % "10.1.3"
+    )
+  )
--- src/main/scala/options/EitherExample.scala
+++ src/main/scala/options/EitherExample.scala
@@ -1,11 +1,29 @@
 package options
 
+import akka.http.scaladsl.model.{IllegalUriException, Uri}
+
+import scala.language.postfixOps
+import scala.util.Try
+
 object EitherExample extends App {
 
+  println("\nChecking division of numbers:")
   println(divideXByY(4, 2))
   println(divideXByY(12, 0))
-
   divideXByY(2, 0) match {
+    case Left(s) => println(s)
+    case Right(s) => println(s)
+  }
+
+  println("\nChecking URLs:")
+  val urlBad = "http://abcd.com/this is not a good URL"
+  val urlGood = "http://abcd.com/this_is_a_good_URL"
+
+  checkUri(urlBad) match {
+    case Left(s) => println(s)
+    case Right(s) => println(s)
+  }
+  checkUri(urlGood) match {
     case Left(s) => println(s)
     case Right(s) => println(s)
   }
@@ -14,4 +32,14 @@ object EitherExample extends App {
     if (y == 0) Left("Cannot divide by 0")
     else Right(x / y)
   }
+
+  def checkUri(uri: String): Either[String, String] = {
+    Try {
+      Uri(uri)
+      Left("You entered a good URL")
+    } recover {
+      case e: IllegalUriException =>
+        Right("You entered a bad URL, causing the following exception: \n" + e)
+    }
+  } get
 }
