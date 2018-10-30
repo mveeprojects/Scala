436f5102dbe6a1343d231c94a24fc172330a950c
Mark Vallis <markvallis1987@Gmail.com>
2018-10-30 17:21:17 +0000

    Adding some collection.map examples

--- src/main/scala/mapsAndFlatmaps/Maps.scala
+++ src/main/scala/mapsAndFlatmaps/Maps.scala
@@ -12,29 +12,31 @@ object Maps {
     mapExampleTwo()
     mapExampleThree()
     mapExampleFour()
+    mapExampleFive()
   }
 
   def mapExampleOne(): Unit = {
     println("*** Maps - Example One ***")
     println("\nList:")
-    val numTimesTwoList = numList.map(elementVal => elementVal*2)
+    val numTimesTwoList = numList.map(elementVal => elementVal * 2)
     for (element <- numTimesTwoList) println(element)
 
     println("\nArray:")
-    val numTimesTwoArray = numList.map(elementVal => elementVal*2)
+    val numTimesTwoArray = numList.map(elementVal => elementVal * 2)
     for (element <- numTimesTwoArray) println(element)
 
     println("\nSet:")
-    val numTimesTwoSet = numList.map(elementVal => elementVal*2)
+    val numTimesTwoSet = numList.map(elementVal => elementVal * 2)
     for (element <- numTimesTwoSet) println(element)
   }
 
   def mapExampleTwo(): Unit = {
     println("\n*** Maps - Example Two ***")
-    val numTimesTwoList = numList.map(_*2)
+    val numTimesTwoList = numList.map(_ * 2)
     for (element <- numTimesTwoList) println(element)
     println()
   }
+
   def mapExampleThree(): Unit = {
     println("\n*** Maps - Example Three ***")
     val numGreaterThanTwoList = numList.map(elementVal => myFunction(elementVal))
@@ -42,17 +44,27 @@ object Maps {
     println()
   }
 
-  def myFunction(x: Int): Option[Int] = if(x > 2) Some(x) else None
+  def myFunction(x: Int): Option[Int] = if (x > 2) Some(x) else None
 
   def mapExampleFour(): Unit = {
     println("\n*** Maps - Example Four ***")
     myMap.map { keyValue: (String, Int) =>
       keyValue match {
-        case (key, value) => (key, value*2)
+        case (key, value) => (key, value * 2)
       }
     }
-    for ((k, v) <- myMap){
+    for ((k, v) <- myMap) {
       println(k + ", " + v)
+    }
+  }
+
+  def mapExampleFive(): Unit = {
+    println("\n*** Maps - Example Five ***")
+    val aList = myMap.map {
+      case (key, value) => value * 2
+    }.toList
+    for (value <- aList) {
+      println(value)
     }
   }
 }
