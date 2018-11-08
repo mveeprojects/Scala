c95292aeb38fb87470477b58bd42b9f2a2eda36b
Mark Vallis <markvallis1987@Gmail.com>
2018-11-08 15:02:20 +0000

    Updated map and flatmap examples

--- src/main/scala/mapsAndFlatmaps/MapVsFlatmap.scala
+++ src/main/scala/mapsAndFlatmaps/MapVsFlatmap.scala
@@ -7,6 +7,7 @@ object MapVsFlatmap {
   def main(args: Array[String]): Unit = {
     mapBasicConcept()
     flatMapBasicConcept()
+    mapThenFlattenProof()
   }
 
   def mapBasicConcept(): Unit = {
@@ -18,6 +19,12 @@ object MapVsFlatmap {
   def flatMapBasicConcept(): Unit = {
     println("\nFlatmaps are maps that are then flattened to a lower level (array of char in this case)")
     val result: List[Char] = nameList.flatMap(_.toLowerCase)
+    println(result)
+  }
+
+  def mapThenFlattenProof(): Unit = {
+    println("\nTo prove this theory, here's an example of running map and then flatten seperately")
+    val result = nameList.map(_.toLowerCase).flatten
     println(result)
   }
 }
---
+++ src/main/scala/mapsAndFlatmaps/README.md
@@ -0,0 +1,5 @@
+# Maps and Flatmaps
+
+Map vs. FlatMap examples taken from:
+
+https://alvinalexander.com/scala/collection-scala-flatmap-examples-map-flatten
