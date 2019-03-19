7d12c54ba2aea79e05171a916fe7dc62b679f44c
Mark Vallis <markvallis1987@Gmail.com>
2019-03-19 16:56:56 +0000

    added another basic futures example

---
+++ src/main/scala/futures/TimingFutureExample.scala
@@ -0,0 +1,29 @@
+package futures
+
+import com.typesafe.scalalogging.LazyLogging
+
+import scala.concurrent.ExecutionContext.Implicits.global
+import scala.concurrent.Future
+import scala.util.{Failure, Random, Success}
+
+object TimingFutureExample extends App with LazyLogging {
+
+  myFancyFunction()
+  Thread.sleep(10)
+
+  def myFancyFunction(): Unit ={
+    val myFutureThing = Future {
+      Random.nextInt(500)
+    }
+
+    myFutureThing.onComplete {
+      case Success(value) => {
+        val starttime = System.currentTimeMillis()
+        println(s"value: $value")
+        println(s"took ${System.currentTimeMillis() - starttime}ms to complete")
+      }
+      case Failure(exception) =>
+        println("damn")
+    }
+  }
+}
