82bc3324fac3ee2fd2bf96a4d2c148edd00f94ce
MarkVee87 <markvallis1987@Gmail.com>
2019-04-15 22:50:17 +0100

    Added copy example

---
+++ src/main/scala/general/CopyExample.scala
@@ -0,0 +1,15 @@
+package general
+
+case class Person(name: String, age: Int) {
+  override def toString: String = {
+    s"$name is $age years old"
+  }
+}
+
+object CopyExample extends App {
+  val person1 = Person("Mark", 32)
+  val person2 = person1.copy(age = 33)
+
+  println(person1.toString())
+  println(person2.toString())
+}
