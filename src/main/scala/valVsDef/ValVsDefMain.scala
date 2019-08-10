8ae9d2a58b82b86f1459939f7de2fac34add069c
MarkVee87 <markvallis1987@Gmail.com>
2019-08-10 13:55:55 +0100

    added val vs def example

---
+++ src/main/scala/valVsDef/ValVsDefMain.scala
@@ -0,0 +1,13 @@
+package valVsDef
+
+object ValVsDefMain extends App {
+
+  // different initialisation
+  val myFilterConditionVal = (i: Int) => i % 2 == 0
+  def myFilterConditionDef(i: Int): Boolean = i % 2 == 0
+
+  // same usage against collections
+  val numberSeq = Seq(1, 2, 3, 4)
+  println(s"val version ${numberSeq.filter(myFilterConditionVal)}")
+  println(s"def version ${numberSeq.filter(myFilterConditionDef)}")
+}
