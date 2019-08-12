2fa6a5050f289bb42fcdbedca33af7410c31e96e
MarkVee87 <markvallis1987@Gmail.com>
2019-08-12 21:59:11 +0100

    added more in depth partially applied function example

--- src/main/scala/partiallyAppliedFunctions/HigherOrderPAFMain.scala
+++ src/main/scala/partiallyAppliedFunctions/HigherOrderPAFMain.scala
@@ -2,15 +2,33 @@ package partiallyAppliedFunctions
 
 object HigherOrderPAFMain extends App {
 
-  // create a 'first class' function
-  val addTwoStrings = (a: String, b: String) => a.toInt + b.toInt
+  exampleOne()
+  exampleTwo()
 
-  // create a 'higher order' function
-  val myHOFunc = (string1: String, string2: String, func: (String, String) => Int) => func(string1, string2)
+  def exampleOne(): Unit = {
+    // create a 'first class' function
+    val addTwoStrings = (a: String, b: String) => a.toInt + b.toInt
 
-  // create a 'partially applied function' based on the higher order function
-  val partiallyAppliedMyHOFunc = myHOFunc(_: String, _: String , addTwoStrings)
+    // create a 'higher order' function
+    val myHOFunc = (string1: String, string2: String, func: (String, String) => Int) => func(string1, string2)
 
-  // call the partially applied function
-  println(partiallyAppliedMyHOFunc("12", "34"))
+    // create a 'partially applied function' based on the higher order function
+    val partiallyAppliedMyHOFunc = myHOFunc(_: String, _: String, addTwoStrings)
+
+    // call the partially applied function
+    println(partiallyAppliedMyHOFunc("12", "34"))
+  }
+
+  def exampleTwo(): Unit = {
+    // FCF
+    val sayHelloFCF = (firstName: String, lastName: String) => s"Hi, my name is $firstName $lastName"
+
+    // HOF
+    val myHOFuncc = (stringA: String, stringB: String, func: (String, String) => String) => func(stringA, stringB)
+
+    // PAF
+    val sayHiMyNameIs = myHOFuncc(_: String, _: String, sayHelloFCF)
+
+    println(sayHiMyNameIs("Mark", "Vallis"))
+  }
 }
