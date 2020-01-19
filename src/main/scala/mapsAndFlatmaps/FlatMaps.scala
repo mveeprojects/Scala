f60fdc747fb31f75230108f6afc449ece23d2be9
mva13 <markvallis1987@Gmail.com>
2020-01-19 20:08:56 +0000

    Added more flatmap examples

---
+++ src/main/scala/mapsAndFlatmaps/FlatMaps.scala
@@ -0,0 +1,43 @@
+package mapsAndFlatmaps
+
+object FlatMaps extends App {
+
+  val stringList = List("apple", "banana", "orange")
+
+  println("Flatmap - think map then flatten or \"mapFlat\"")
+  stringList.flatMap(_.toUpperCase).foreach(println)
+
+  println("Map")
+  val mappedList = stringList.map(_.toUpperCase)
+  mappedList.foreach(println)
+
+  println("Flatten")
+  val flattenedList = stringList.flatten
+  flattenedList.foreach(println)
+
+  println("\"Manual\" Flatmap")
+  stringList.map(_.toUpperCase).flatten.foreach(println)
+}
+
+
+object FlatMapsOnOptions extends App {
+
+  val maybeIntList = List("1", "seven", "6", "12")
+
+  maybeIntList.flatMap(toInt).foreach(println)
+
+  def toInt(s: String): Option[Int] = {
+    try {
+      Some(s.toInt)
+    } catch {
+      case _: Exception => None
+    }
+  }
+}
+
+object FlatMapListOfSeqs extends App {
+  val intList = List(1, 2, 3)
+  intList.map(x => Seq(x - 1, x, x + 1)).foreach(x => print(s"$x "))
+  println
+  intList.flatMap(x => Seq(x - 1, x, x + 1)).foreach(x => print(s"$x "))
+}
